fc1d3b7785a788fd9db1e4448bce42c1a430af9c
smhigley <sarahmhigley@gmail.com>
2016-10-16 15:39:30 -0700

    page title needs h1

--- wp-content/themes/syfc-theme/page.php
+++ wp-content/themes/syfc-theme/page.php
@@ -5,7 +5,7 @@
 
 <div class="row content">
 	<?php while (have_posts()) : the_post(); ?>
-		<?php the_title(); ?>
+		<h1><?php the_title(); ?></h1>
     <?php the_content(); ?>
   <?php endwhile; ?>
 </div>
