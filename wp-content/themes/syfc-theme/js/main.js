87466cdc24a9a30cf647480eaa44c4eff60a9c42
smhigley <sarahmhigley@gmail.com>
2016-10-15 18:07:31 -0700

    starting google maps api js

--- wp-content/themes/syfc-theme/js/main.js
+++ wp-content/themes/syfc-theme/js/main.js
@@ -3,5 +3,21 @@
 
   // google maps
   var key = 'AIzaSyCvvaeIF0uoGD-0TAt92sv2x3aTZGqgKbg';
+  function getCoordsFromAddress(address, callback) {
+    //
+  }
+  function displayMapBox(el, coords) {
+    var geocoder, map;
+    geocoder = new google.maps.Geocoder();
+    var mapOptions = {
+      zoom: 12,
+      center: new google.maps.LatLng(47.6062, 122.3321),
+      //'styles': styles,
+    };
+    map = new google.map.Map(el, mapOptions);
+    if (geocoder) {
+      geocoder.geocode({address: address});
+    }
+  }
 
 }(jQuery));
